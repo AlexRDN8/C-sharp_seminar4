c939463e86da992a8a1a9c19ec21bd7ac1ee5953
AlexRDN8 <alexrodin8@bk.ru>
2022-12-06 14:39:46 +0300

    added task 2

---
+++ task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4->24 5->120
+
+Console.WriteLine("Введите число: ");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int N);
+int multiplicationOfNumber = getMultiplicationOfNumber(N);
+Console.WriteLine(multiplicationOfNumber);
+
+
+if (!isParsedN || N < 1)
+{
+    Console.WriteLine("incorrect value");
+    return;
+}
+
+int getMultiplicationOfNumber(int N)
+{
+    int result = 1;
+    for (int i = 1; i <= N; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
