1c5b32b3b49c62026fdf3485ac5e133759e2df98
AlexRDN8 <alexrodin8@bk.ru>
2022-12-06 14:14:04 +0300

    added task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3 78 -> 2 89126 -> 5
+
+Console.WriteLine("Введите число");
+bool isParsedNum = int.TryParse(Console.ReadLine(), out int Num);
+if (!isParsedNum)
+{
+    Console.WriteLine("Incorrect value");
+}
+
+int GetAccountOfFigures (int Num)
+{
+    int accountOfFigures = (int)Math.Floor(Math.Log10(Num)+1);
+    return accountOfFigures;
+}
+
+Console.WriteLine(GetAccountOfFigures (Num));
